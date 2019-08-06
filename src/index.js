7d82634c5b6ea245e5a00c8af8575f9690dfe62d
Константин Лысенко <gameron27@gmail.com>
2019-08-06 18:41:37 +0300

    инициализация проекта

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// JS
+import './js/'
+import './js/jquery.min.js'
+
+// SCSS
+import './assets/sass/main.sass'
+
+// CSS (example)
+// import './assets/css/main.css'
+
+// Vue.js
+//window.Vue = require('vue')
+
+// Vue components (for use in html)
+//Vue.component('example-component', require('./components/Example.vue').default)
+
+// Vue init
+/*const app = new Vue({
+  el: '#app'
+})*/
